8f39655bd9698e72600e3780c68398850df3a9e6
hackertaco <imsa1224@naver.com>
2020-07-21 16:49:07 +0900

    feat: ⚡todos

---
+++ src/containers/TodosContainer.js
@@ -0,0 +1,14 @@
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Todos from "../components/Todos";
+import { addTodo, toggleTodo } from "../modules/todos";
+function TodosContainer() {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  const onCreate = useCallback((text) => dispatch(addTodo(text)), [dispatch]);
+  const onToggle = useCallback((id) => dispatch(toggleTodo(id)), [dispatch]);
+  return <Todos onCreate={onCreate} onToggle={onToggle} todos={todos} />;
+}
+
+export default TodosContainer;
