0dcef3c65230ee09a4ac3180273d1f20f0ed4b04
hackertaco <imsa1224@naver.com>
2020-07-21 18:46:22 +0900

    fix: 🔥give store props to provider

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = createStore(rootReducer, composeWithDevtools);
 console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
